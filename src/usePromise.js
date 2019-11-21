141882ffe39dfa5db79e8196a5cbb592457f1cc2
Emiliano Bovetti <emiliano.bovetti@gmail.com>
2019-11-21 16:11:36 +0100

    alpha-feat: usePromise inputs array

--- src/usePromise.js
+++ src/usePromise.js
@@ -1,20 +1,18 @@
 import { useState, useEffect } from 'react';
 import { Result, Ok } from '@fpc/result';
-import { expectPromise } from '@fpc/types';
 
-export const usePromise = promise => {
-  expectPromise(promise);
-
+export const usePromise = (promise, inputs = [promise]) => {
   const [result, setResult] = useState(Ok);
+  const resultPromise = Result(promise);
 
   useEffect(() => {
     let cancelled = false;
 
-    Result(promise).then(res => cancelled || setResult(res));
+    resultPromise.then(res => cancelled || setResult(res));
 
     /* eslint-disable-next-line no-return-assign */
     return () => cancelled = true;
-  }, [promise]);
+  }, inputs);
 
   return result;
 };
