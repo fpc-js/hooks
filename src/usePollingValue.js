7f0b1a7380322cafe63c013d5a53106120b29e29
Emiliano Bovetti <emiliano.bovetti@gmail.com>
2020-04-24 11:21:04 +0200

    fix: usePollingValue repeated function invocation

--- src/usePollingValue.js
+++ src/usePollingValue.js
@@ -1,3 +1,9 @@
+import { useState } from 'react';
+import { expectFunction } from '@fpc/types';
 import { useLazy } from './useLazy';
 
-export const usePollingValue = fn => useLazy(fn, fn());
+export const usePollingValue = fn => {
+  const [initialValue] = useState(expectFunction(fn));
+
+  return useLazy(fn, initialValue);
+};
